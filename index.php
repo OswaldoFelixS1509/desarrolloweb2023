dd8ca2b2544cb85b996961e09357530916d0edc4
OswaldoFelixS1509 <106107045+OswaldoFelixS1509@users.noreply.github.com>
2023-09-13 21:22:06 -0700

    Login form y index Co-authored-by: David-Unreal <David-Unreal@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+    <?php
+        include "layout/head.inc";
+    ?>
+    <body>
+        <header class="header">
+            <div class="profilePicture">
+                <img src="images/person.png" class="pfp" alt="">
+            </div>
+            <div class="username">
+                usuario
+            </div>
+            <div class="welcomeLabel">
+                <div class="welcome">
+                    <h1>
+                        BIENVENIDO
+                    </h1>
+                </div>
+                
+            </div>
+        </header>
+    </body>
+</html>
